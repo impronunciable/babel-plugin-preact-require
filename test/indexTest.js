866c6ead7976c648e82e03014af0412c7199ef4c
Dan Zajdband <dan.zajdband@gmail.com>
2017-03-04 10:54:38 -0500

    updated tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,26 +1,26 @@
-const babel = require('babel-core');
-const assert = require('assert');
+const babel = require('babel-core')
+const assert = require('assert')
 
-let reactPlugin;
-let transform;
+let preactPlugin
+let transform
 
 try {
-  reactPlugin = require('../lib-cov/index').default; // eslint-disable-line import/no-unresolved
+  preactPlugin = require('../lib-cov/index').default // eslint-disable-line import/no-unresolved
 } catch (error) {
-  reactPlugin = require('../src/index').default;
+  preactPlugin = require('../src/index').default
 }
 
-describe('babel-plugin-react', () => {
+describe('babel-plugin-preact', () => {
   beforeEach(() => {
     transform = code => babel.transform(code, {
-      plugins: ['syntax-jsx', reactPlugin],
-    }).code;
-  });
+      plugins: ['syntax-jsx', preactPlugin]
+    }).code
+  })
 
-  it('should return transpiled code with required React', () => {
+  it('should return transpiled code with required Preact h function', () => {
     const transformed = transform('export default class Component {render() {return <div />}}');
 
-    assert.equal(transformed, 'import React from "react";\nexport default class Component {\n  render() {\n    return <div />;\n  }\n}');
+    assert.equal(transformed, 'import { h } from "preact";\nexport default class Component {\n  render() {\n    return <div />;\n  }\n}');
   });
 
   it('should return not transpiled code', () => {
@@ -29,16 +29,11 @@ describe('babel-plugin-react', () => {
     assert.equal(transformed, 'console.log("hello world");');
   });
 
-  it('should check that plugin does not import React twice', () => {
-    const transformed = transform('class Component{render(){return <div/>}} class Component2{render(){return <div />}}');
+  it('should check that plugin does not import Preact twice', () => {
+    const transformed = transform('class Component{render(){return <div/>}} class Component2{render(){return <div />}}')
 
-    assert.equal(transformed, 'import React from "react";\nclass Component {\n  render() {\n    return <div />;\n  }\n}'
-      + 'class Component2 {\n  render() {\n    return <div />;\n  }\n}');
-  });
-
-  it('should does not replace users import on plugins import', () => {
-    const transformed = transform('import React from"react/addons"\nclass Component{render(){return <div/>}}');
-
-    assert.equal(transformed, 'import React from "react/addons";\nclass Component {\n  render() {\n    return <div />;\n  }\n}');
-  });
-});
+    assert.equal(transformed,
+      'import { h } from "preact";\nclass Component {\n  render() {\n    return <div />;\n  }\n}' +
+      'class Component2 {\n  render() {\n    return <div />;\n  }\n}')
+  })
+})
